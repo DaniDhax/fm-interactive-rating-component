e94d869b42d56594a0153d48dce672d4e5b878d5
DaniDhax <DaniDhax@users.noreply.github.com>
2023-08-18 11:56:03 +0200

    Design 5

--- app.js
+++ app.js
@@ -7,9 +7,22 @@
 
 const ratingState = document.querySelector(".state-rating");
 const thankyouState = document.querySelector(".state-thankyou");
+const ratingButton = document.querySelector(".button-rating");
 const submitButton = document.querySelector(".button-submit");
+const spanValor = document.getElementById("valor");
+
+let valorBoton;
 
 submitButton.addEventListener("click", () => {
   ratingState.style.display = "none";
-  thankyouState.style.display = "block"
+  thankyouState.style.display = "block";
+  spanValor.textContent = valorBoton;
+});
+
+const botones = document.querySelectorAll(".button-rating");
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    valorBoton = boton.textContent;
+  });
 });
